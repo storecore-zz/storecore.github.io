85b6f88b95815ab98e6f98b232d81b1d7a3486a9
Ward van der Put <Ward.van.der.Put@gmail.com>
2020-04-22 12:43:15 +0200

    Minify CSS

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -158,7 +158,7 @@ flush();
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500%7CNoto+Serif:400,400i,700%7CRoboto+Mono:500&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:900&amp;text=Store&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <style amp-custom><?php require 'storecore.css' ?></style>
+    <style amp-custom><?php require 'storecore.min.css' ?></style>
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 
     <link rel="canonical" href="<?= $canonical ?>">
